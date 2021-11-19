a296a818ef01d6adb798e6d0bd024f6a7bd47037
Shan Zafar <bitf18545@pucit.edu.pk>
2021-11-19 23:00:04 +0500

    Another Commit

--- app/src/main/java/com/shanzafar/practice/MainActivity.java
+++ app/src/main/java/com/shanzafar/practice/MainActivity.java
@@ -1,13 +1,30 @@
 package com.shanzafar.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String MSG = "com.shanzafar.practice.ORDER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void PlaceOrder(View view)
+    {
+        Intent intent = new Intent(this, OrderActivity.class);
+        EditText et1 = findViewById(R.id.et1);
+        EditText et2 = findViewById(R.id.et2);
+        EditText et3 = findViewById(R.id.et3);
+        String msg = et1.getText().toString() + ","
+                + et2.getText().toString() + "&"
+                + et3.getText().toString();
+        intent.putExtra(MSG, msg);
+        startActivity(intent);
     }
 }
